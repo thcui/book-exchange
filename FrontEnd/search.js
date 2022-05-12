ccd0aeed69fb5dd2c739483ff0ee6626f4898e3a
Zhenrui Chen <chenzhenrui@dyn-160-39-171-147.dyn.columbia.edu>
2022-05-12 11:54:19 -0400

    convey info and create sort functions

--- FrontEnd/search.js
+++ FrontEnd/search.js
@@ -1,3 +1,6 @@
+var searchResult = []
+var condition_option = [ "Poor", "Fair", "Good", "Very Good", "As New"]
+
 // function to do the search and display the images in front end
 function searchPhoto() {
   var query = document.getElementById("searchContent").value;
@@ -23,11 +26,46 @@ function searchPhoto() {
   }
   
   apigClient.searchGet(params, body, additionalParams).then(function (response) {
+    //TODO: how to update info in the frontend part
     result = response.data;
+    searchResult = result;
     console.log(result.length);
-    console.log(result[0]);
     this.info = result;
+    console.log(this.info);
   }).catch(function (result) {
     console.log(result);
   })
+}
+
+function filter() {
+  this.info = searchResult;
+  filterByOrder();
+  filterByCategory();
+  filterByCondition();
+}
+
+function filterByOrder() {
+
+}
+
+function filterByCategory() {
+  var newInfo = [];
+  for (var i = 0; i < this.info.length; i++) {
+    if (this.info[i].genres.indexOf(this.display.category) > -1) {
+      newInfo.push(this.info[i]);
+    }
+  }
+  this.info = newInfo;
+}
+
+function filterByCondition() {
+  var newInfo = [];
+  for (var i = 0; i< this.info.length; i++) {
+    book_value = condition_option.indexOf(this.info[i].condition);
+    re = condition_option.indexOf(this.display.min_requirement);
+    if (book_value >= re) {
+      newInfo.push(this.info[i]);
+    }
+  }
+  this.info = newInfo;
 }
