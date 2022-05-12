495348a1e32ac6ce3d977f705fcf5b16a528bbec
thcui <albhang@126.com>
2022-05-11 20:48:25 -0400

    add sending message panel

--- FrontEnd/accountManagement.js
+++ FrontEnd/accountManagement.js
@@ -1,4 +1,4 @@
-var login_user_name = 'albert-new4';
+var login_user_name = null;
 var user_credit = null;
 var user_info_cognito = null;
 
--- FrontEnd/chatbox.js
+++ FrontEnd/chatbox.js
@@ -1,3 +1,25 @@
+var current_recipient=""
+
+
+function sendMessage(){
+    params={}
+    var apigClient = apigClientFactory.newClient();
+    var additionalParams = {};
+    var body = {
+        "sender":login_user_name,
+        "recipient":current_recipient,
+        "content":document.getElementById("message_input").value
+    };
+    console.log('???')
+    closeForm()
+    apigClient.messagePost(params, body, additionalParams)
+        .then(res => {
+            console.log(res)
+            this.messages = res.data.messages
+        });
+}
+
+
 function openForm() {
     document.getElementById("myForm").style.display = "block";
 }
